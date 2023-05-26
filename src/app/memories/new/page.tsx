584c794ed390396491f3b38afb65cb306589ee67
Bene-Hur Pessoa <benehurp@gmail.com>
2023-05-26 14:32:55 -0300

    feat: added list to memories

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function NewMemory() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4 p-16">
       <Link href={'/'} className="flex items-center gap-1 text-sm">
         <ChevronLeft className="h-4 w-4" />
         voltar à timeline
