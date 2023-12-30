a32ba40f306282340cd3697fe13db64946438842
Bene-Hur Pessoa <benehurp@gmail.com>
2023-12-30 20:57:37 -0300

    fix: deploy settings

---
+++ src/@types/environment.d.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NEXT_PUBLIC_GITHUB_CLIENT_ID: string
+    NEXT_PUBLIC_BASE_URL: string
+  }
+}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.110:3333',
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
 })
